49926bc6c0e428d6d612ca842a8448db0baf22da
ryryette <rvt.univ@gmail.com>
2025-10-08 01:24:07 +0200

    system.sh

--- system.sh
+++ system.sh
@@ -1,2 +1,22 @@
-"Atelier 4"
+#"Atelier 4"
+# Script "infos système" de l'exercice 4
+#-----------------
+
+#!/bin/bash
+
+echo "# SYSTEM"
+echo "OS: $(lsb_release -d | cut -f2) ($(uname -m))"
+
+echo "# HARDWARE"
+echo "CPU:"
+echo "CPU(s): $(nproc)"
+echo "Cores: $(grep -c '^core id' /proc/cpuinfo)"
+echo "Total Memory: $(free -h | awk '/Mem:/ {print $2}')"
+echo "Total Disk Space: $(df -h --total | awk '/total/ {print $2}')"
+echo "$(grep 'model name' /proc/cpuinfo | head -1 | cut -d: -f2 | xargs)"
+
+echo "# CURRENT STATE"
+echo "Used Memory: $(free -h | awk '/Mem:/ {print $3 " / " $2}')"
+echo "Swap: $(free -h | awk '/Swap:/ {print $3 " / " $2}')"
+echo "Used Disk Space: $(df -h --total | awk '/total/ {print $5}')"
 
